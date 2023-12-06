65f029211d2fc4b83395b0d7c825dceeb6ca796b
Rhaqim <anusiemj@gmail.com>
2023-12-06 11:06:01 +0100

    parsing info from the webpage

--- src/core/content.js
+++ src/core/content.js
@@ -5,76 +5,12 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
 	// Check if the message instructs to show the popup
 	if (message.action === "showPopup") {
 		// Add your logic to show the popup or interact with the DOM here
+
+		// highlight the first paragraph
+		document.querySelector("p").style.backgroundColor = "yellow";
+		document.querySelector("p").style.color = "black";	
+
+
 		console.log("Show popup logic in content script");
-
-		let link = window.location.href;
-
-		let jobSite = parseJobSite(link);
-		console.log(jobSite);
 	}
 });
-
-function parseJobSite(website) {
-	if (website.includes("linkedin")) {
-		return parseLinkedin(website);
-	} else if (website.includes("indeed")) {
-		return parseIndeed(website);
-	} else if (website.includes("glassdoor")) {
-		return parseGlassdoor(website);
-	} else {
-		return null;
-	}
-}
-
-function parseLinkedin(link) {
-	let jobTitle = document.querySelector(".topcard__title").innerText;
-	let company = document.querySelector(".topcard__org-name-link").innerText;
-	let location = document.querySelector(".topcard__flavor").innerText;
-	let description = document.querySelector(".description__text").innerText;
-
-	return {
-		jobTitle,
-		company,
-		location,
-		description,
-		link,
-	};
-}
-
-function parseIndeed(link) {
-	let jobTitle = document.querySelector(
-		".jobsearch-JobInfoHeader-title"
-	).innerText;
-	let company = document.querySelector(
-		".jobsearch-InlineCompanyRating"
-	).innerText;
-	let location = document.querySelector(
-		".jobsearch-InlineCompanyRating"
-	).innerText;
-	let description = document.querySelector(
-		".jobsearch-JobComponent-description"
-	).innerText;
-
-	return {
-		jobTitle,
-		company,
-		location,
-		description,
-		link,
-	};
-}
-
-function parseGlassdoor(link) {
-	let jobTitle = document.querySelector(".jobTitle").innerText;
-	let company = document.querySelector(".jobHeaderCompanyInfo").innerText;
-	let location = document.querySelector(".jobHeaderCompanyInfo").innerText;
-	let description = document.querySelector(".jobDescriptionContent").innerText;
-
-	return {
-		jobTitle,
-		company,
-		location,
-		description,
-		link,
-	};
-}
